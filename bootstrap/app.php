d2e0fc7834d6a8b69bd86879b4ba1cdafa04a9c0
elmaaref <abdelrahimabuelmaaref@gmail.com>
2025-11-08 15:24:39 +0200

    Register the middleware {CheckWorkerStatus} in app file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
+use App\Http\Middleware\CheckWorkerStatus;
 
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->append(CheckWorkerStatus::class);
         $middleware->prepend(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
